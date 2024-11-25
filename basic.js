ef37774ae14c97a566c419f65c505f2b05ed7a9e
sudhanshu pathak <pji101329@gmail.com>
2024-11-25 10:11:57 +0530

    commit

---
+++ basic.js
@@ -0,0 +1,11 @@
+const accountId=133
+let email = "su@123";
+var pass="1233"
+accCity="jaipur"
+
+email="12@email"
+pass="22"
+console.log(accountId);
+/*
+*/ 
+console.table([accountId,email,pass,accCity])
